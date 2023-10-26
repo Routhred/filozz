e08e29d6a176e0f207d5434dfe9d7b522e97be60
Routhred <romain.serpollet@gmail.com>
2023-10-26 13:28:20 +0200

    Modification nom app

--- App/app/src/main/java/com/example/filozz/MainActivity.kt
+++ App/app/src/main/java/com/example/filozz/MainActivity.kt
@@ -3,16 +3,15 @@ package com.example.filozz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.filozz.ui.theme.TestTheme
+import com.example.filozz.ui.theme.FilozzTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestTheme {
+            FilozzTheme {
                 navigation()
-                }
             }
         }
     }
-
+}
